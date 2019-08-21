e8f7d0cdf9e08c24abb13002ab82f93f7cf62bd3
Gyuris Csaba <gycsaba@vasvari.hu>
2019-08-21 13:51:03 +0200

    Valós típus és kifejezések

---
+++ 01egeszek/Peldaprogram/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    a=5;
+    cout<<"a="<<a<<endl;
+
+    cout<<"13/5="<<13/5<<endl;
+    cout<<"13%5="<<13%5<<endl;
+    int b=2;
+    b++;
+    cout<<"b="<<b<<endl;
+    b*=4;
+    cout<<"b="<<b<<endl;
+    cout<<"234/123="<<234/123;
+
+    return 0;
+}
---
+++ 02valosak/valossak/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#include <math.h>
+
+int main()
+{
+    float a;
+    a=5.25;
+    cout<<"a="<<a<<endl;
+
+    double b;
+    b=5.25;
+    cout<<"b="<<b<<endl;
+
+    cout<<"sqrt(4)="<<sqrt(4)<<endl;
+    cout<<"pow(2,8)="<<pow(2,8)<<endl;
+    cout<<"sin(3.14)="<<sin(3.14)<<endl;
+
+    return 0;
+}
